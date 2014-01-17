e2e4455728dbc9ce516014a8751c4bfc8c5b567a
Jacek Złydach <temporal.pl@gmail.com>
2014-01-17 19:11:17 +0100

    Stubbing lights mode.

---
+++ logic/lights.c
@@ -0,0 +1,19 @@
+// driver
+#include "display.h"
+#include "ports.h"
+
+// logic
+#include "lights.h"
+
+void sx_lights(u8 line) {
+}
+
+void mx_lights(u8 line) {
+	//set up the BPM mode
+}
+
+
+void display_lights(u8 line, u8 update) {
+
+}
+
